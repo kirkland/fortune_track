7512e07be81a68f492685888a45c06781016ea3e
Robert Kaufman <rob.m.kaufman@gmail.com>
2012-11-20 23:26:37 -0500

    Some validations.

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,9 @@
 class LineItem < ActiveRecord::Base
   belongs_to :transaction
   belongs_to :account
+
+  validates_presence_of :debit_in_cents
+  validates_presence_of :credit_in_cents
+  validates_presence_of :account
+  validates_presence_of :transaction
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,13 @@
 class Transaction < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
 
-  validate :
+  validate :debits_equals_credits
+
+  private
+
+  def debits_equals_credits
+    if line_items.sum(&:debit_in_cents) != line_items.sum(&:credit_in_cents)
+      errors.add(:line_items, 'total debits must equal total credits')
+    end
+  end
 end
