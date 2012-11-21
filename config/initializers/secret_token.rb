dd8457f08fc072ea534404da38124ba3c76ab301
Robert Kaufman <rob.m.kaufman@gmail.com>
2012-11-20 22:36:51 -0500

    Initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonalAccounting::Application.config.secret_token = 'f4ca04b3f19c5fc76a70c901a0a163491bd02da68ce1586e5e6fba5083bbb514d1b6dffbbcab25e1d82ffd01095d51f6b838d46a6fc6efe26cc5a59d4b95e6b2'
