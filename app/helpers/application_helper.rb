7f2685c02e1048db27730ff8a394131225a302d5
Robert Kaufman <rob.m.kaufman@gmail.com>
2012-12-01 20:18:50 -0500

    Cleanup.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
   def display_date(date_object)
+    return '(no date)' if date_object.nil?
+
     date_object.strftime('%Y-%m-%d')
   end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,6 +4,8 @@ class Transaction < ActiveRecord::Base
   has_many :line_items, dependent: :destroy, inverse_of: :transaction, order: 'id ASC'
 
   validate :debits_equals_credits
+  validate :validates_has_line_item
+  validates :date, presence: true
 
   after_save :delete_empty_line_items
 
@@ -18,6 +20,12 @@ class Transaction < ActiveRecord::Base
   def delete_empty_line_items
     line_items.each do |line_item|
       line_item.destroy if line_item.debit_in_cents == 0 && line_item.credit_in_cents == 0
+    end
+  end
+
+  def validates_has_line_item
+    if line_items.count == 0
+      errors.add(:line_items, 'must have at least one line item')
     end
   end
 end
