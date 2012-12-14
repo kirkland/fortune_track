e90ea0c4427093602dc0393c857b842eba2b23d6
Robert Kaufman <rob.m.kaufman@gmail.com>
2012-12-14 18:12:03 +0700

    Finish update_full_name logic.

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,17 +2,31 @@ class Account < ActiveRecord::Base
   attr_accessible :name, :parent_account_id
 
   belongs_to :parent_account, class_name: 'Account'
+  has_many :child_accounts, class_name: 'Account', foreign_key: 'parent_account_id'
   has_many :line_items
+
+  before_save :update_related_full_names!
 
   def update_full_name
     if parent_account_id.present?
-      "#{parent_account.full_name}:#{name}"
+      self.full_name = "#{parent_account.full_name}:#{name}"
     else
-      name
+      self.full_name = name
     end
   end
 
   def self.find_or_create_with_hierarchy(full_name)
     parts = full_name.split(':')
+  end
+
+  def update_related_full_names!
+    if name_changed?
+      update_full_name
+
+      child_accounts.each do |acct|
+        acct.update_related_full_names!
+        acct.save!
+      end
+    end
   end
 end
