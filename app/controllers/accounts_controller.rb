ea7458a5e5edfda0a946008f618bb7d29f50668d
Robert Kaufman <rob.m.kaufman@gmail.com>
2012-12-14 21:22:23 +0700

    Fix up accounts index, use indentation.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,44 +1,20 @@
 class AccountsController < ApplicationController
-  # GET /accounts
-  # GET /accounts.json
   def index
-    @accounts = Account.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @accounts }
-    end
+    @accounts = Account.where(parent_account_id: nil).all
   end
 
-  # GET /accounts/1
-  # GET /accounts/1.json
   def show
     @account = Account.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @account }
-    end
   end
 
-  # GET /accounts/new
-  # GET /accounts/new.json
   def new
     @account = Account.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @account }
-    end
   end
 
-  # GET /accounts/1/edit
   def edit
     @account = Account.find(params[:id])
   end
 
-  # POST /accounts
-  # POST /accounts.json
   def create
     @account = Account.new(params[:account])
 
