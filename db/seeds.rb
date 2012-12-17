22046252d1dfa6ef370772109da2776274c61385
Robert Kaufman <rob.m.kaufman@gmail.com>
2012-12-17 11:37:42 +0700

    Some more seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-expenses = Account.create(name: 'Expenses')
-
+# Assets
 assets = Account.create(name: 'Assets')
 current = Account.create(name: 'Current', parent_account_id: assets.id)
 Account.create(name: 'Cash', parent_account_id: current.id)
 
+# Equity
 equity = Account.create(name: 'Equity')
 Account.create(name: 'Opening Balance', parent_account_id: equity.id)
 
+# Expenses
+expenses = Account.create(name: 'Expenses')
+food = Account.create(name: 'Food', parent_account_id: expenses.id)
+Account.create(name: 'Restaurant', parent_account_id: food.id)
+Account.create(name: 'Groceries', parent_account_id: food.id)
+
+# Income
 income = Account.create(name: 'Income')
 Account.create(name: 'Salary', parent_account_id: income.id)
 
+# Liabilities
 liabilities = Account.create(name: 'Liabilities')
 Account.create(name: 'Credit Cards', parent_account_id: liabilities.id)
