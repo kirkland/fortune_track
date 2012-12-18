4af0df45134bc50ee339809a3cb9cb1e465dcbde
Robert Kaufman <rob.m.kaufman@gmail.com>
2012-12-18 10:26:42 +0700

    Transaction seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require File.join(Rails.root, '/test/factories')
+
 def create_accounts(data, parent_account_id=nil)
   if data.is_a? String
     Account.create(name: data, parent_account_id: parent_account_id)
@@ -12,3 +14,8 @@ end
 
 data = YAML.load(File.read(File.join(Rails.root, 'db/initial_accounts.yml')))
 data.each { |x| create_accounts(x) }
+
+# Some seed transaction data.
+10.times { FactoryGirl.create(:asset_expense_transaction) }
+5.times { FactoryGirl.create(:liability_expense_transaction) }
+2.times { FactoryGirl.create(:income_transaction) }
--- test/factories.rb
+++ test/factories.rb
@@ -1,19 +1,65 @@
+require 'factory_girl'
+
 FactoryGirl.define do
   factory :transaction do
     description Faker::Lorem.words(3).join(' ').capitalize + '.'
     date { (Time.now - rand(10).days).to_date }
 
-    after :build do |t|
-      amount = rand(1000)
-      li1 = build(:line_item, transaction: t, debit_in_cents: amount)
-      li2 = build(:line_item, transaction: t, credit_in_cents: amount)
+    factory :asset_expense_transaction do
+      after :build do |t|
+        amount = rand(1000)
+        li1 = build(:expense_line_item, transaction: t, debit_in_cents: amount)
+        li2 = build(:asset_line_item, transaction: t, credit_in_cents: amount)
 
-      t.line_items << li1
-      t.line_items << li2
+        t.line_items << li1
+        t.line_items << li2
+      end
+    end
+
+    factory :liability_expense_transaction do
+      after :build do |t|
+        amount = rand(1000)
+        li1 = build(:expense_line_item, transaction: t, debit_in_cents: amount)
+        li2 = build(:liability_line_item, transaction: t, credit_in_cents: amount)
+
+        t.line_items << li1
+        t.line_items << li2
+      end
+    end
+
+    factory :income_transaction do
+      after :build do |t|
+        amount = rand(1000)
+        li1 = build(:asset_line_item, transaction: t, debit_in_cents: amount)
+        li2 = build(:income_line_item, transaction: t, credit_in_cents: amount)
+
+        t.line_items << li1
+        t.line_items << li2
+      end
     end
   end
 
-  factory :line_item, aliases: [:debit_line_item] do
-    account Account.first
+  factory :line_item do
+    account { Account.all.sample }
+
+    factory :expense_line_item do
+      account { Account.all.select{|x| x.full_name =~ /^Expense/}.sample }
+    end
+
+    factory :asset_line_item do
+      account { Account.all.select{|x| x.full_name =~ /^Asset/}.sample }
+    end
+
+    factory :liability_line_item do
+      account { Account.all.select{|x| x.full_name =~ /^Liabilities/}.sample }
+    end
+
+    factory :income_line_item do
+      account { Account.all.select{|x| x.full_name =~ /^Income/}.sample }
+    end
+
+    factory :equity_line_item do
+      account { Account.all.select{|x| x.full_name =~ /^Equity/}.sample }
+    end
   end
 end
