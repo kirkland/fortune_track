d4062cd87a75dd2e27f882c6ebaae797cad5f484
Robert Kaufman <rob.m.kaufman@gmail.com>
2012-12-23 21:23:17 +0700

    Only show accounts with a balance, but always show top-level accounts.

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -3,7 +3,15 @@ module AccountsHelper
     "#{account.depth * 20}px"
   end
 
+  def show_account?(account)
+    account.debit_total_with_children != account.credit_total_with_children
+  end
+
   def show_children?(account)
     true
+  end
+
+  def has_visible_children?(account)
+    account.child_accounts.present? && account.child_accounts.any?{ |x| show_account?(x) }
   end
 end
