dda498a073a64303400efc5226bd58cda4a36cf6
Robert Kaufman <rob.m.kaufman@gmail.com>
2012-12-28 15:17:41 +0700

    Add HTTP basic authentication for production.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  if Rails.env.production?
+    http_basic_authenticate_with :name => Credentials['site']['username'],
+      :password => Credentials['site']['password']
+  end
 end
