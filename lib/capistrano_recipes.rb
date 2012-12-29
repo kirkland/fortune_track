2a75b221b464061eddc68a18d914bdb2bd312f02
Robert Kaufman <rob.m.kaufman@gmail.com>
2012-12-29 13:57:36 +0700

    Change locations of real and sample credentials files.

--- lib/capistrano_recipes.rb
+++ lib/capistrano_recipes.rb
@@ -1,11 +1,11 @@
 Capistrano.configuration(:must_exist).load do
   desc 'Upload secret credentials file to server.'
   task :upload_credentials, role: :app do
-    file = "#{ENV['HOME']}/.fortune_track_credentials.yml"
+    file = File.join File.dirname(__FILE__), '../config/credentials.yml'
     if File.exists? file
       upload file, '/u/apps/fortune_track/current/config/credentials.yml'
     else
-      puts 'Warning: no real credentials file found. Your app will be accessible with the default credentials.'
+      raise "No credentials file at #{file} found."
     end
   end
   after :deploy, 'upload_credentials'
