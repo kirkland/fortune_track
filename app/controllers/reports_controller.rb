94c9f70ece965505d2ba52603816e4067324bfef
Robert Kaufman <rob.m.kaufman@gmail.com>
2013-01-01 16:19:18 +0700

    Make expense report configurable.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,8 @@
 class ReportsController < ApplicationController
   def expense_report
-    @report_rows = Reports::ExpenseReport.new(params[:start_date].try(:to_date),
-      params[:end_date].try(:to_date)).report
+    @start_date = params[:start_date].try(:to_date)
+    @end_date = params[:end_date].try(:to_date)
+
+    @report_rows = Reports::ExpenseReport.new(@start_date, @end_date).report
   end
 end
