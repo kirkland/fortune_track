4e6cc6d21eb8dac1d62377a37ac43e88b8ec4459
Robert Kaufman <rob.m.kaufman@gmail.com>
2013-01-06 18:18:34 -0500

    Finish interface to allow importing new transactions.

--- app/controllers/account_imports_controller.rb
+++ app/controllers/account_imports_controller.rb
@@ -4,6 +4,23 @@ class AccountImportsController < ApplicationController
   end
 
   def create
-    
+    importer_class = params[:importer_class].constantize
+
+    if !AccountParsers::ALL.include? importer_class
+      raise 'That is not an importer class, hacker.'
+    end
+
+    p = importer_class.new
+
+    if params[:file].present?
+      p.raw_data = params[:file].read
+      new_transactions = p.create_new_transactions
+    else
+      new_transactions = p.download_and_create_transactions
+    end
+
+    flash[:notice] = "Added #{new_transactions.count} new transactions."
+
+    redirect_to root_path
   end
 end
