51bce462311f77ac68d6f325f85200aa0876057b
Robert Kaufman <rob.m.kaufman@gmail.com>
2013-01-06 21:05:13 -0500

    Make sure old account gets balances updated when a line item is reassigned.

--- app/observers/account_balances_observer.rb
+++ app/observers/account_balances_observer.rb
@@ -4,9 +4,17 @@ class AccountBalancesObserver < ActiveRecord::Observer
   def after_save(record)
     if record.is_a? LineItem
       record.account.update_balances!
+
+      if record.account_id_changed?
+        Account.find(record.account_id_was).update_balances!
+      end
     else
       record.line_items.each do |line_item|
         line_item.account.update_balances!
+
+        if record.account_id_changed?
+          Account.find(record.account_id_was).update_balances!
+        end
       end
     end
   end
