9c65f90ac61041fa9e0452fd33ca558e83c6f3fa
Robert Kaufman <rob.m.kaufman@gmail.com>
2013-01-11 21:02:17 -0500

    Split out Central Bank savings and checking.

--- lib/account_parsers/central_bank_parser.rb
+++ lib/account_parsers/central_bank_parser.rb
@@ -6,7 +6,7 @@ module AccountParsers
     include BankAccount
 
     def primary_account
-      @primary_account ||= Account.all.detect{|x| x.name =~ /Central Bank/}
+      @primary_account ||= Account.all.detect{|x| x.name =~ /Central Bank:Checking/}
     end
 
     def build_transactions
