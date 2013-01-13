6f8fea2e4e4a8670425b25e6efb3c045115ed4a4
Robert Kaufman <rob.m.kaufman@gmail.com>
2013-01-13 16:01:12 -0500

    Clean up a bit logic to announce outdated imports.

--- app/models/account_import.rb
+++ app/models/account_import.rb
@@ -1,10 +1,26 @@
 class AccountImport < ActiveRecord::Base
   # TODO: validation that importer_class_name is valid
 
+  OUTDATED_THRESHOLD = 2.days
+
   after_create :start_import
 
   scope :successful, where(successful: true)
   scope :recent, order('started_at DESC')
+
+  def self.outdated
+    outdated = {}
+
+    AccountImporters::ALL.each do |importer_class_name|
+      recent = AccountImport.successful.recent.where(importer_class_name: importer_class_name)
+        .try(:first)
+      if recent.nil? || recent.started_at < Time.now - OUTDATED_THRESHOLD
+        outdated[importer_class_name] = recent.try(:started_at)
+      end
+    end
+
+    outdated
+  end
 
   private
 
