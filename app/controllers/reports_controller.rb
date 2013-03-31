e16eecc4e7b7ae74360e703d29dc3457cce4fbdf
Robert Kaufman <rob.m.kaufman@gmail.com>
2013-03-31 19:52:53 -0400

    Change nothing I hope.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,9 +1,13 @@
 class ReportsController < ApplicationController
   def expense_report
-    @start_date = params[:start_date].try(:to_date) || Date.today.beginning_of_month
-    @end_date = params[:end_date].try(:to_date) || Date.today
+    respond_to do |format|
+      format.html do
+        @start_date = params[:start_date].try(:to_date) || Date.today.beginning_of_month
+        @end_date = params[:end_date].try(:to_date) || Date.today
 
-    @report_rows = Report::ExpenseReport.new(@start_date, @end_date).run
+        @report_rows = Report::ExpenseReport.new(@start_date, @end_date).run
+      end
+    end
   end
 
   def income_report
