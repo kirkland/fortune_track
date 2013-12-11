8557d5288a0c54bb0b7c59a4808727e75267eb35
Robert Kaufman <rob.m.kaufman@gmail.com>
2013-12-11 08:12:44 -0500

    Bold account name if we're on its page

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,4 +1,14 @@
 module TransactionsHelper
+  def account_name(account)
+    # If we're viewing this account's list of transactions
+    if account == @account
+      "<strong>".html_safe + account.full_name + "</strong>".html_safe
+#      "<strong>#{account.full_name}</strong>".html_safe
+    else
+      account.full_name
+    end
+  end
+
   # TODO: Truncate to reasonable length, add elipsis, add 'expand' link
   def transaction_description(transaction)
     transaction.description.presence || '(no description)'
